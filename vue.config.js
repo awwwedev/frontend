5abfd6d737009307428e6f34b2c0c491a9cb8721
Rydal <i.am.alex2k@gmail.com>
2021-05-05 17:29:29 +0300

    added path to common

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
+import path from 'path'
+
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/frontend/'
         : '/',
     transpileDependencies: ['vuex-module-decorators'],
     configureWebpack: {
+        resolve: {
+            alias: {
+                "@common": path.resolve(__dirname, 'src/git-modules/common/src')
+            }
+        }
     }
 }
